9aea3a3a547ba6f8895118961d540592e243ab3a
abdo01234567 <abdoalrhman.ayman0@gmail.com>
2024-12-23 01:10:10 +0200

    Update Abctract_method.py

--- cood/Abctract_method.py
+++ cood/Abctract_method.py
@@ -1,25 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  5 20:50:56 2023
-
-@author: ascom
-"""
-
-"""
-An abstract class can be considered a blueprint for other classes. 
-It allows you to create a set of methods that must be created within 
-any child classes built from the abstract class.
-
-A class that contains one or more abstract methods is called an abstract class. 
-An abstract method is a method that has a declaration but does not have an implementation.
-
-By default, Python does not provide abstract classes. Python comes with a module that 
-provides the base for defining Abstract Base classes(ABC) and that module name is ABC.
-"""
-
-# Python program showing 
-# abstract base class work 
-
 from abc import ABC, abstractmethod 
 
 
